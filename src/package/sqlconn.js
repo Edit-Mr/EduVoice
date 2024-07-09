9e56d378f03c830cacab5d8c9bd28919d9a96b34
Elvis Mao <info@elvismao.com>
2024-07-09 08:32:52 +0800

    refactor: Update DB_HOST variable name in sqlconn.js

--- src/package/sqlconn.js
+++ src/package/sqlconn.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mariadb = require("mariadb");
 
 const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
+    DB_HOST: process.env.DB_HOST,  
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,

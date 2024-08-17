de383d4085f89ecd46af6d6d34758edbda8d5cc9
Elvis Mao <info@elvismao.com>
2024-08-18 01:45:46 +0800

    404

--- src/front.js
+++ src/front.js
@@ -1,30 +1,39 @@
 /** @format */
 
-// import express from "express";
-// import path from "path";
-// const app = express();
-// import { fileURLToPath } from "url";
-
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
-const { fileURLToPath } = require("url");
+const { fileURLToPath } = require('url');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Set EJS as the templating engine
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/static", express.static(path.join(__dirname, "static")));
-app.use(express.static(path.join(__dirname, "public")));
+// homepage to index view
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
-// when enter /[view-name] in the url, it will render the view
-app.get("/:view", (req, res) => {
-    res.render(`${req.params.view}`);
+app.get('/:view', (req, res) => {
+  const viewPath = path.join(__dirname, 'views', `${req.params.view}.ejs`);
+
+  if (fs.existsSync(viewPath)) {
+    res.render(req.params.view);
+  } else {
+    res.status(404).render('404');
+  }
+});
+
+//404 page
+app.use((req, res) => {
+  res.status(404).render('404');
 });
 
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });

268cbc7d451e4b865f32181e54e56f69d7bc524b
Elvis Mao <info@elvismao.com>
2024-11-09 20:35:43 +0800

    Update sqlconn.js

--- src/package/sqlconn.js
+++ src/package/sqlconn.js
@@ -6,6 +6,7 @@ const pool = mariadb.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
     connectionLimit: 5,
 });
 

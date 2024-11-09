7678db16fae70b763e81601a8b524e8b44a104ee
Each Chen <iach526526@gmail.com>
2024-11-09 21:29:52 +0800

    sql connect

--- src/package/sqlconn_init.js
+++ src/package/sqlconn_init.js
@@ -6,6 +6,7 @@ const init_pool=mariadb.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     // dons't  ensure the database exist or not
+    port: process.env.DB_PORT,
     connectionLimit: 5,
 });  
 module.exports = init_pool;

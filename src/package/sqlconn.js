aec61a0ee05d93128d6a552450a52e33909e3a61
Each Chen <iach526526@gmail.com>
2024-11-10 21:45:33 +0800

    env name

--- src/package/sqlconn.js
+++ src/package/sqlconn.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 const mariadb = require("mariadb");
 
 const pool = mariadb.createPool({
-    DB_HOST: process.env.DB_HOST,  
+    host: process.env.DB_HOST,  
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     connectionLimit: 5,
 });
-console.log("db env",process.env.DB_HOST,process.env.DB_USER,process.env.DB_PASSWORD,process.env.DB_NAME,process.env.DB_PORT);
+console.log("db env",process.env.DB_HOST,process.env.DB_USER,
+    process.env.DB_PASSWORD,process.env.DB_NAME,process.env.DB_PORT);
 module.exports = pool;
--- src/package/sqlconn_init.js
+++ src/package/sqlconn_init.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const mariadb = require("mariadb");
 
 const init_pool=mariadb.createPool({
-    DB_HOST: process.env.DB_HOST,  
+    host: process.env.DB_HOST,  
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     // dons't  ensure the database exist or not
